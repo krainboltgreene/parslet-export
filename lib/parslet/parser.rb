9c4c47274a2e55f86a4b49c78485567440f59267
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-08-07 21:16:57 -0700

    We want these input/output methods documented.

--- lib/parslet/parser.rb
+++ lib/parslet/parser.rb
@@ -1,5 +1,6 @@
 module Parslet
   class Parser
+    # This turns a Parslet::Parser into a Hash
     def to_hash
       {
         "atom" => "root",
@@ -7,8 +8,10 @@ module Parslet
       }
     end
 
+    # This turns a Hash into a Parslet::Parser
     def self.from_hash(hash)
       new.tap do |parser|
+        # Parsers have to have a #root method
         parser.define_singleton_method(hash["atom"]) do
           hash["parslet"].to_parslet
         end
